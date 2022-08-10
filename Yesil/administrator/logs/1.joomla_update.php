14e1da78d0aef027058ee03f2e3185803d4d8ddb
Doğukan Başer <neyazcam2000@gmail.com>
2022-08-11 02:25:49 +0300

    a
    a
    
    Initial commit

---
+++ Yesil/administrator/logs/1.joomla_update.php
@@ -0,0 +1,24 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-04-27 09:16:15 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-04-27T09:16:15+00:00	INFO ::1	update	Update started by user Super User (970). Old version is 3.10.6.
+2022-04-27T09:16:18+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.10.8-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20220427%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220427T091609Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=a321b207dd11505cab7e51f309f4c812e07ecb3891a1364908d07a4ae56c5bd1.
+2022-04-27T09:17:17+00:00	INFO ::1	update	File Joomla_3.10.8-Stable-Update_Package.zip downloaded.
+2022-04-27T09:17:17+00:00	INFO ::1	update	Starting installation of new version.
+2022-04-27T09:17:33+00:00	INFO ::1	update	Finalising installation.
+2022-04-27T09:17:33+00:00	INFO ::1	update	Ran query from file 3.10.7-2022-02-20. Query text: DELETE FROM `#__postinstall_messages` WHERE `title_key` = 'COM_ADMIN_POSTINSTALL.
+2022-04-27T09:17:33+00:00	INFO ::1	update	Ran query from file 3.10.7-2022-03-18. Query text: ALTER TABLE `#__users` ADD COLUMN `authProvider` VARCHAR(100) NOT NULL DEFAULT '.
+2022-04-27T09:17:33+00:00	INFO ::1	update	Deleting removed files and folders.
+2022-04-27T09:17:35+00:00	INFO ::1	update	Cleaning up after installation.
+2022-04-27T09:17:35+00:00	INFO ::1	update	Update to version 3.10.8 is complete.
+2022-05-19T06:53:32+00:00	INFO ::1	update	Update started by user Super User (454). Old version is 3.10.8.
+2022-05-19T06:53:35+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.10.9-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20220519%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220519T065333Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=a3c2bac15cddce804f94414c9e212e31acc89bfb6c02d9133516c3fbf93e5f84.
+2022-05-19T06:53:41+00:00	INFO ::1	update	File Joomla_3.10.9-Stable-Update_Package.zip downloaded.
+2022-05-19T06:53:41+00:00	INFO ::1	update	Starting installation of new version.
+2022-05-19T06:53:59+00:00	INFO ::1	update	Finalising installation.
+2022-05-19T06:53:59+00:00	INFO ::1	update	Deleting removed files and folders.
+2022-05-19T06:54:01+00:00	INFO ::1	update	Cleaning up after installation.
+2022-05-19T06:54:01+00:00	INFO ::1	update	Update to version 3.10.9 is complete.
---
+++ Yesil/administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-06-02 17:34:03 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-06-02T17:34:03+00:00	INFO ::1	joomlafailure	Kullanıcı adı ve parola eşleşmiyor ya da henüz bir hesabınız yok.
+2022-06-03T10:06:19+00:00	INFO ::1	joomlafailure	Kullanıcı adı ve parola eşleşmiyor ya da henüz bir hesabınız yok.
